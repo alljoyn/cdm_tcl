12e86fa1ceef044bd228ef6fb99e4e06da5ed493
Anthony Shipman <anthony@two-bulls.com>
2016-11-25 14:22:36 +1100

    ASAHAE-28 Array implementation for models
    Added a new array implementation
    
    Change-Id: I149f6fab602b52e50f0162ca3b33bc411d3539b0
    Signed-off-by: Nathaniel Garbutt <nathaniel@two-bulls.com>

---
+++ inc/utils/CDM_Array.h
@@ -0,0 +1,45 @@
+/******************************************************************************
+ * Copyright AllSeen Alliance. All rights reserved.
+ *
+ *    Permission to use, copy, modify, and/or distribute this software for any
+ *    purpose with or without fee is hereby granted, provided that the above
+ *    copyright notice and this permission notice appear in all copies.
+ *
+ *    THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *    WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *    MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *    ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *    WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *    ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *    OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ ******************************************************************************/
+
+#ifndef ALLJOYN_CDM_ARRAY_H
+#define ALLJOYN_CDM_ARRAY_H
+
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define MK_ARRAY(TYPE, NAME) typedef struct {TYPE* elems; size_t numElems;} Array_ ## NAME; FREE_ARRAY(TYPE, NAME)
+#define FREE_ARRAY(TYPE, NAME) extern void Free_ ## NAME(Array_ ## NAME* array);
+
+#define MAKE_ARRAYS \
+MK_ARRAY(bool, Bool) \
+MK_ARRAY(char*, string) \
+MK_ARRAY(uint8_t, uint8) \
+MK_ARRAY(int16_t, int16) \
+MK_ARRAY(uint16_t, uint16) \
+MK_ARRAY(int32_t, int32) \
+MK_ARRAY(uint32_t, uint32) \
+MK_ARRAY(int64_t, int64) \
+MK_ARRAY(uint64_t, uint64) \
+MK_ARRAY(double, double)
+
+MAKE_ARRAYS
+
+#undef MK_ARRAY
+#undef FREE_ARRAY
+#undef MAKE_ARRAYS
+
+#endif //ALLJOYN_CDM_ARRAY_H
