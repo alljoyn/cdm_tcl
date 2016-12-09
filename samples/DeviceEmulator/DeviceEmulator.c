edbb9a036af624e00cb64faba4d6f1966eb0e1d4
Nathaniel Garbutt <nathaniel@two-bulls.com>
2016-12-09 14:53:55 +1100

    ASABASE-953 Device Emulator
    Removed hardcoded certificates and use the new security util
    
    Change-Id: I43344cb22449f01fb724cb1b792cc91fbad3e461
    Signed-off-by: Nathaniel Garbutt <nathaniel@two-bulls.com>

--- samples/DeviceEmulator/DeviceEmulator.c
+++ samples/DeviceEmulator/DeviceEmulator.c
@@ -20,8 +20,54 @@
 #include <ajtcl/aj_status.h>
 #include <ajtcl/cdm/utils/CDM_System.h>
 #include <ajtcl/cdm/utils/CDM_AboutData.h>
+#include <ajtcl/cdm/utils/CDM_Security.h>
+
+#include <ajtcl/cdm/CdmControllee.h>
+
+#include <ajtcl/cdm/interfaces/operation/OnOffStatusInterface.h>
 
 #include "DeviceConfig.h"
+
+#include "Models/operation/OnOffStatusModelImpl.h"
+
+/**
+ * Security authentication suites.
+ * - DO NOT REUSE THESE VALUES. They are copied from ajtcl/samples/secure/SecureServiceECDHE.c.
+ */
+
+static const uint32_t suites[4] = { AUTH_SUITE_ECDHE_ECDSA, AUTH_SUITE_ECDHE_SPEKE, AUTH_SUITE_ECDHE_PSK, AUTH_SUITE_ECDHE_NULL };
+//static const char pem_prv[] = {
+//    "-----BEGIN EC PRIVATE KEY-----"
+//        "MDECAQEEICCRJMbxSiWUqj4Zs7jFQRXDJdBRPWX6fIVqE1BaXd08oAoGCCqGSM49"
+//        "AwEH"
+//        "-----END EC PRIVATE KEY-----"
+//};
+//
+//static const char pem_x509[] = {
+//    "-----BEGIN CERTIFICATE-----"
+//        "MIIBuDCCAV2gAwIBAgIHMTAxMDEwMTAKBggqhkjOPQQDAjBCMRUwEwYDVQQLDAxv"
+//        "cmdhbml6YXRpb24xKTAnBgNVBAMMIDgxM2FkZDFmMWNiOTljZTk2ZmY5MTVmNTVk"
+//        "MzQ4MjA2MB4XDTE1MDcyMjIxMDYxNFoXDTE2MDcyMTIxMDYxNFowQjEVMBMGA1UE"
+//        "CwwMb3JnYW5pemF0aW9uMSkwJwYDVQQDDCAzOWIxZGNmMjBmZDJlNTNiZGYzMDU3"
+//        "NzMzMjBlY2RjMzBZMBMGByqGSM49AgEGCCqGSM49AwEHA0IABGJ/9F4xHn3Klw7z"
+//        "6LREmHJgzu8yJ4i09b4EWX6a5MgUpQoGKJcjWgYGWb86bzbciMCFpmKzfZ42Hg+k"
+//        "BJs2ZWajPjA8MAwGA1UdEwQFMAMBAf8wFQYDVR0lBA4wDAYKKwYBBAGC3nwBATAV"
+//        "BgNVHSMEDjAMoAoECELxjRK/fVhaMAoGCCqGSM49BAMCA0kAMEYCIQDixoulcO7S"
+//        "df6Iz6lvt2CDy0sjt/bfuYVW3GeMLNK1LAIhALNklms9SP8ZmTkhCKdpC+/fuwn0"
+//        "+7RX8CMop11eWCih"
+//        "-----END CERTIFICATE-----"
+//};
+
+static const char ecspeke_password[] = "1234";
+
+static AJ_Status CreateInterfaces()
+{
+    AJ_Status status = AJ_OK;
+
+    status = Cdm_AddInterface("/cdm/lamp", ON_OFF_STATUS, intfDescOperationOnOffStatus, &intfHandlerOperationOnOffStatus, GetOnOffStatusModel());
+
+    return status;
+}
 
 int main(int argc, char *argv[])
 {
@@ -38,6 +84,9 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    Cdm_SetSuites(suites, 4);
+    Cdm_EnableSPEKE(ecspeke_password);
+
     DEM_Config *config = DEM_CreateConfig(argv[1]);
     const CDM_AboutDataBuf aboutData = CDM_CreateAboutDataFromXml(config->aboutData);
 
@@ -45,15 +94,32 @@ int main(int argc, char *argv[])
     status = CDM_SystemInit(&iconParams);
     if (status != AJ_OK)
     {
-        fprintf(stderr, "SystemInit failed: %d\n", status);
+        fprintf(stderr, "SystemInit failed: %s\n", AJ_StatusText(status));
         retVal = 1;
         goto CLEANUP;
     }
 
+    CreateInterfaces();
+
     CDM_SetDefaultRoutingNodeParams(&routingNodeParams);
     status = CDM_SystemConnect(&routingNodeParams, &bus);
+    if (status != AJ_OK)
+    {
+        fprintf(stderr, "SystemConnect failed: %s\n", AJ_StatusText(status));
+        retVal = 1;
+        goto CLEANUP;
+    }
+
+    status = Cdm_EnableSecurity(&bus.bus, NULL);
+    if (status != AJ_OK)
+    {
+        fprintf(stderr, "Cdm_EnableSecurity failed: %s\n", AJ_StatusText(status));
+        retVal = 1;
+        goto SHUTDOWN;
+    }
 
 
+SHUTDOWN:
     CDM_SystemStop();
 
 CLEANUP:
